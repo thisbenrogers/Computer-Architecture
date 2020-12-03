cd34cc7cf1a14f86347b2310e9c6b3b03b2676fa
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-12-02 20:58:18 -0600

    Implements CPU constructor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.bytes = [0] * 256
+        self.registers = [0] * 8
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
